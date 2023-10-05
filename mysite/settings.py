9ec9cdf4e5e6c4d871dbac0e921e0683f4e12c22
admin <admin@admin.com>
2023-10-06 00:53:27 +0300

    solved security issues

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3o+!nso1afr81v_&^07)l04_84ocyx@sxj+8y@+1o1%+e2@svg'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -45,6 +48,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap4',
     'django.contrib.humanize',
 
+    # for google auth
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -81,8 +85,8 @@ SOCIALACCOUNT_PROVIDERS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_FROM = 'sdordiuk@gmail.com'
-EMAIL_HOST_USER = 'sdordiuk@gmail.com'
-EMAIL_HOST_PASSWORD = 'cmjagsktlrynlwgq'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
