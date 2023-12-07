b3c38d583eb266170868eff5bff05a21bd738d9a
Olderestin <sdordiuk@gmail.com>
2023-12-07 20:03:40 +0200

    Add redis for cache

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'),
+    path('post/new/', cache_page(60*60*24)(views.post_new), name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('search/', views.Search.as_view(), name='search'),
     path('post/<int:pk>/delete/', views.post_delete, name='post_delete')
--- mysite/settings.py
+++ mysite/settings.py
@@ -216,3 +216,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+    }
+}
